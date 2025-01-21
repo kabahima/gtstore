d11f3e5b71dd3f3384b7dd2c35006b9793e9da5a
Ernest Kabahima <43174238+kabahima@users.noreply.github.com>
2025-01-21 16:55:13 +0300

    fix : add static settings in the core/urls.py file

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import home_view
+from core import settings
 
 
 urlpatterns = [
@@ -10,4 +11,8 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
 
     path('', home_view, name='home'),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
